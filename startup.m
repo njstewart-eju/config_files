5ff36e5bd162a569dd2869ac3173b403889f2516
njstewart-eju <zephyr853@gmail.com>
2022-04-08 14:41:04 +0100

    add Matlab startup script for defining folders to add to path

---
+++ startup.m
@@ -0,0 +1,33 @@
+
+%% Set-up paths independent of OS
+if ispc; userdir= getenv('USERPROFILE');
+else; userdir= getenv('HOME');
+end
+
+% Github
+addpath(strcat(userdir,filesep,'Documents',filesep,'github',...
+    filesep,'Matlab_MRI'));
+addpath(strcat(userdir,filesep,'Documents',filesep,'github',...
+    filesep,'matlab-schemer'));
+
+% Gitlab
+addpath(strcat(userdir,filesep,'Documents',filesep,'gitlab',...
+    filesep,'ge-rawdata',filesep,'read_mr_latest'));
+addpath(strcat(userdir,filesep,'Documents',filesep,'gitlab',...
+    filesep,'ge-rawdata',filesep,'orchestra-sdk-2.0-1.matlab'));
+addpath(strcat(userdir,filesep,'Documents',filesep,'gitlab',...
+    filesep,'philips-rawdata'));
+addpath(strcat(userdir,filesep,'Documents',filesep,'gitlab',...
+    filesep,'mns-research-pack'));
+addpath(strcat(userdir,filesep,'Documents',filesep,'gitlab',...
+    filesep,'tensor_toolbox'));
+addpath(strcat(userdir,filesep,'Documents',filesep,'gitlab',...
+    filesep,'zte_recon'));
+
+addpath(strcat(userdir,filesep,'OneDrive',filesep,'matlab_SupportFiles',...
+        filesep,'Misc'));
+
+if ispc
+%% Add WSL2 Ubuntu-installed BART to path
+    run('Z:\home\njs\bart\startup.m');
+end
