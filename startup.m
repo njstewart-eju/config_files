17742d4cc43800a4f936c8f161e22b353fec93c8
njstewart-eju <zephyr853@gmail.com>
2022-07-17 23:40:41 +0100

    clean up

--- startup.m
+++ startup.m
@@ -1,7 +1,6 @@
 %STARTUP - script that runs when Matlab starts!
 %
 % NJS (TUoS) 2022
-
 disp('Running NJS startup.m...');
 
 %% Set-up paths independent of OS
@@ -12,19 +11,31 @@ end
 % Github
 disp('Loading Github paths...');
 base_string = strcat(userdir,filesep,'Documents',filesep,'github');
-addpath(strcat(base_string,filesep,'Matlab_MRI'));
-addpath(strcat(base_string,filesep,'matlab-schemer'));
+paths = {strcat(filesep,'Matlab_MRI'),...
+    strcat(filesep,'matlab-schemer')};
+for p = 1:length(paths)
+    path = strcat(base_string,paths{p});
+    if exist(path,'dir')
+        addpath(path);
+    end
+end
 
 % Gitlab
 disp('Loading Gitlab paths...');
 base_string = strcat(userdir,filesep,'Documents',filesep,'gitlab');
-addpath(strcat(base_string,filesep,'ge-rawdata',filesep,'read_mr_latest'));
-addpath(strcat(base_string,filesep,'ge-rawdata',filesep,'orchestra-sdk-2.0-1.matlab'));
-addpath(strcat(base_string,filesep,'philips-rawdata'));
-addpath(strcat(base_string,filesep,'mns-research-pack'));
-addpath(strcat(base_string,filesep,'tensor_toolbox'));
-addpath(strcat(base_string,filesep,'zte_recon'));
-addpath(strcat(base_string,filesep,'plab'));
+paths = {strcat(filesep,'ge-rawdata',filesep,'read_mr_latest'),...
+    strcat(filesep,'ge-rawdata',filesep,'orchestra-sdk-2.0-1.matlab'),...
+    strcat(filesep,'ge-rawdata',filesep,'orchestra-sdk-2.0-1.matlab',filesep,'Scripts'),...
+    strcat(filesep,'philips-rawdata'),...
+    strcat(filesep,'mns-research-pack'),...
+    strcat(filesep,'tensor_toolbox'),...
+    strcat(filesep,'zte_recon'),...
+    strcat(filesep,'plab')};
+for p = 1:length(paths)
+    path = strcat(base_string,paths{p});
+    if exist(path,'dir')
+        addpath(path);
+    end
+end
 
-clear;
 disp('Done...');
