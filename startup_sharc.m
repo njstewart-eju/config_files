6bf4910d81d7713fe4278ac63fb5c8071dc0fbc3
njstewart-eju <zephyr853@gmail.com>
2023-09-30 22:37:29 +0100

    same edits as normal startup.m

--- startup_sharc.m
+++ startup_sharc.m
@@ -8,21 +8,12 @@ disp('Running NJS startup.m...');
 % Github
 disp('Loading Github paths...');
 base_string = '/shared/plus1/user/md1njst/github';
-paths = {strcat(filesep,'ge-rawdata',filesep,'read_mr_latest'),...
-    strcat(filesep,'ge-rawdata',filesep,'orchestra-sdk-2.0-1.matlab'),...
-    strcat(filesep,'ge-rawdata',filesep,'gw_coils_files'),...
-    strcat(filesep,'ge-rawdata',filesep,'orchestra-sdk-2.0-1.matlab',filesep,'Scripts'),...
-    strcat(filesep,'Matlab_MRI'),...
-    strcat(filesep,'mns-research-pack'),...
-    strcat(filesep,'ute-zte-utilities'),...
-    strcat(filesep,'matlab-schemer'),...
-    strcat(filesep,'fasta-matlab'),...
-    strcat(filesep,'fasta-matlab',filesep,'solvers')
-    };
+paths = dir(base_string);
+paths = paths(~ismember({paths.name},{'.','..'}));
 for p = 1:length(paths)
-    path = strcat(base_string,paths{p});
+    path = strcat(base_string,paths(p).name);
     if exist(path,'dir')
-        addpath(path);
+        addpath(genpath(path));
     end
 end
 
@@ -33,12 +24,12 @@ schemer_import(strcat(base_string,filesep,'matlab-schemer',filesep,...
 % Gitlab
 disp('Loading Gitlab paths...');
 base_string = '/shared/plus1/user/md1njst/gitlab';
-paths = {strcat(filesep,'tensor_toolbox')
-    };
+paths = dir(base_string);
+paths = paths(~ismember({paths.name},{'.','..'}));
 for p = 1:length(paths)
-    path = strcat(base_string,paths{p});
+    path = strcat(base_string,paths(p).name);
     if exist(path,'dir')
-        addpath(path);
+        addpath(genpath(path));
     end
 end
 
