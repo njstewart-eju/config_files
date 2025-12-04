82ad8fc303eb285eb9739d06c6d0830f389fef25
njstewart-eju <zephyr853@gmail.com>
2025-12-04 16:25:42 +0000

    clean up Matlab startup

--- startup.m
+++ startup.m
@@ -11,21 +11,31 @@ end
 disp('Loading NJS paths...');
 base_string = fullfile(userdir,'Documents','github');
 
-addpath(genpath(fullfile(base_string,'fasta-matlab')));
-addpath(genpath(fullfile(base_string,'ge-rawdata','orchestra-sdk-2.1-1.matlab')));
-addpath(genpath(fullfile(base_string,'ge-rawdata','read_mr_latest')));
-addpath(genpath(fullfile(base_string,'ismrmd')));
 addpath(genpath(fullfile(base_string,'Matlab_MRI')));
-addpath(genpath(fullfile(base_string,'matlab-schemer')));
-addpath(genpath(fullfile(base_string,'mns-research-pack')));
 addpath(genpath(fullfile(base_string,'ute-utilities')));
 addpath(genpath(fullfile(base_string,'teaching-resources')));
+
+%% External Tools
+
+% GE Tools
+addpath(genpath(fullfile(base_string,'ge-rawdata','orchestra-sdk-2.1-1.matlab')));
+addpath(genpath(fullfile(base_string,'ge-rawdata','read_mr_latest')));
+addpath(genpath(fullfile(base_string,'mns-research-pack')));
+% ISMRMD
+addpath(genpath(fullfile(base_string,'ismrmd')));
+% TOPPE: Pulseq interpreter for GE
 addpath(genpath(fullfile(base_string,'toppe')));
+% Fordanic / Forsberg image registrations
+addpath(genpath(fullfile(base_string,'image-registration')));
+% Matlab Schemer
+addpath(genpath(fullfile(base_string,'matlab-schemer')));
+% Fasta algorithm
+addpath(genpath(fullfile(base_string,'fasta-matlab')));
 
 % Matlab-Schemer
 schemer_import(fullfile(userdir,'Documents','github','matlab-schemer','schemes','monokai.prf'));
 
-% plab
+%% plab
 plab_path = '/mnt/xdrive/plus1/slib/dev/plab/';
 if exist(plab_path,'dir')
 	disp('Adding plab to path..');
@@ -35,8 +45,8 @@ else
 end
 
 % Bart
-bart_path = fullfile(base_string,'bart');
-addpath(fullfile(bart_path,'matlab'));
+% bart_path = fullfile(base_string,'bart');
+% addpath(fullfile(bart_path,'matlab'));
 % Assuming bart is in /usr/bin as installed via apt so don't need to set BART_TOOLBOX_PATH.
 % If instead we want to use the bart in bart_path, run the below.
 % setenv('BART_TOOLBOX_PATH', bart_path);
