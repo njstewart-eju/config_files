2f1cfee6673a1eab21a759afe717906c5d692fb5
njstewart-eju <zephyr853@gmail.com>
2026-07-13 17:41:54 +0100

    begin to add basic keybinds and make consistent with qtile

--- config.py
+++ config.py
@@ -125,7 +125,7 @@ keys = [
         desc="Toggle fullscreen on the focused window",
     ),
     Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     # NJS: add backlight controls (JustAGuyLinux)
